0793dd4defcf8bcc101ce99006a574ec19240ab3
Artem Arakcheev <artarakcheev@gmail.com>
2025-01-30 08:14:35 +0300

    Add Code component with IFrame

--- src/Code.js
+++ src/Code.js
@@ -1,24 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   Container,
-  Divider,
-  Header,
-  Segment
 } from 'semantic-ui-react'
+import Iframe from 'react-iframe'
 import Menubar from './components/Menubar'
 
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
+
 const Code = () => {
+  const { height, width } = useWindowDimensions();
+
+  console.log('width:', width, ' height:', height)
   return (
-    <Container>
-      <Menubar />
-      <Divider hidden />
-      <Segment>
-        <Header as='h1'>Code</Header>
-        <p>This is the Code page content.</p>
-      </Segment>
-      <Divider hidden />
-    </Container>
+    <>
+      <Container>
+        <Menubar />
+      </Container>
+      <Iframe url="http://localhost:8080/"
+              width={width}
+              height={height-50}
+              id=""
+              className=""
+              display="block"
+              position="relative"/>
+    </>
   )
 }
 
 export default Code
+
+// AI! I want to have a terminal in the browser in my React.js application that I build. When user opens the Code.js component it sees the terminal window in the browser and can type commands. Every command that the user types goes to the backend API that is executed inside of the Docker container. The terminal is attached to the bash that is executed inside of the docker container. Commands that user inputs in the web terminal gets executed inside of the docker containers on backend. User sees the output of those commands in the browser terminal. How would you build such a system? What npm react.js components would you use? How would you develop the backend on Node.js?
