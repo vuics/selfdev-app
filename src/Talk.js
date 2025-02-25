474b5ff26be650884d77ea3a3ec3f67efa9435ca
Artem Arakcheev <artarakcheev@gmail.com>
2025-02-25 07:18:01 +0300

    Connect with Converse to Prosody from Talk component

--- src/Talk.js
+++ src/Talk.js
@@ -22,44 +22,49 @@ const Talk = () => {
 
   const [ converseRoot, setConverseRoot ] = useState(null)
 
-  // useEffect(() => {
-  //   console.log('converseRoot:', converseRoot)
-  //   window.addEventListener("converse-loaded", function(event) {
-  //     const { converse } = event.detail;
-  //     console.log('converse:', converse)
-  //     converse.initialize({
-  //       // bosh_service_url: 'https://selfdev-prosody.dev.local:5281/bosh/',
+  // TODO: Uncomment? Use this Converse or defined in index.html?
+  //
+  useEffect(() => {
+    console.log('converseRoot:', converseRoot)
+    window.addEventListener("converse-loaded", function(event) {
+      const { converse } = event.detail;
+      console.log('converse:', converse)
+      converse.initialize({
+        // bosh_service_url: 'https://selfdev-prosody.dev.local:5281/bosh/',
+        discover_connection_methods: false,
+        websocket_url: 'wss://selfdev-prosody.dev.local:5281/xmpp-websocket',
+        // root: converseRoot,
+        // view_mode: 'fullscreen',
+        // view_mode: 'embedded',
+        view_mode: 'overlayed',
+        // view_mode: 'mobile',
 
-  //       discover_connection_methods: false,
-  //       websocket_url: 'wss://selfdev-prosody.dev.local:5281/xmpp-websocket',
-
-  //       root: converseRoot,
-  //       // view_mode: 'fullscreen',
-  //       // view_mode: 'embedded',
-  //       view_mode: 'overlayed',
-  //       // view_mode: 'mobile',
-
-  //       show_controlbox_by_default: true,
-  //     });
-  //     setLoading(false)
-  //   });
-  // }, [converseRoot])
+        show_controlbox_by_default: true,
+      });
+      setLoading(false)
+    });
+  }, [converseRoot])
 
 
   return (
     <Container>
-    {/*
+      {/* TODO: Uncomment? Use this Converse or defined in index.html?
+      */}
       <Helmet>
-        <link rel="stylesheet" type="text/css" media="screen" href="/converse/v10.1.8/converse.min.css" />
-        <script src="/converse/v10.1.8/converse.min.js" charset="utf-8"></script>
+        <link rel="stylesheet" type="text/css" media="screen" href="/dist/converse.min.css" />
+        <script src="/dist/converse.min.js" charset="utf-8"></script>
       </Helmet>
-    */}
 
       <Menubar />
 
       <Segment secondary>
-        <converse-root></converse-root>
         {/*
+          TODO: For embedding with script placed in index.html
+        */}
+        <converse-root></converse-root>
+
+        {/*
+          TODO: For overlay or fullscreen view defined in this file
         */}
         <div ref={ (ref) => setConverseRoot(ref) } />
       </Segment>
