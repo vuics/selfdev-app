639280838a284940785ea3c921e27c0c6b7d8d18
Artem Arakcheev <artarakcheev@gmail.com>
2025-02-25 03:56:09 +0300

    Show converse.js in index.html

--- src/Talk.js
+++ src/Talk.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import {Helmet} from "react-helmet"
 // import { isEmpty } from 'lodash'
 // import axios from 'axios'
 import {
@@ -12,6 +13,33 @@ import {
 } from 'semantic-ui-react'
 import Menubar from './components/Menubar'
 import conf from './conf'
+// import { converse } from './converse/v10.1.8/converse.js'
+// import './converse/v10.1.8/converse.js'
+// import './converse/v10.1.8/converse.css'
+// import './converse/v10.1.8/converse.min.js'
+// import './converse/v10.1.8/converse.min.css'
+
+export const useScript = (url, name) => {
+
+  const [lib, setLib] = useState({})
+
+  useEffect(() => {
+    const script = document.createElement('script')
+
+    script.src = url
+    script.async = true
+    script.onload = () => setLib({ [name]: window[name] })
+
+    document.body.appendChild(script)
+
+    return () => {
+      document.body.removeChild(script)
+    }
+  }, [url])
+
+  return lib
+
+}
 
 const Talk = () => {
   // const name = localStorage.getItem('user.firstName') + ' ' +
@@ -19,11 +47,32 @@ const Talk = () => {
 
   const [ loading, setLoading ] = useState(true)
 
+  // const { Stripe } = useScript('https://js.stripe.com/v2/', 'Stripe')
+
+  // root
+
   useEffect(() => {
+    // window.converse && window.converse.initialize({
+    //   bosh_service_url: 'https://conversejs.org/http-bind/', // Please use this connection manager only for testing purposes
+    //   show_controlbox_by_default: true
+    // });
   }, [])
 
   return (
     <Container>
+        {/*
+      <Helmet>
+        <link rel="stylesheet" type="text/css" media="screen" href="/converse/v10.1.8/converse.min.css" />
+        <script src="/converse/v10.1.8/converse.min.js" charset="utf-8"></script>
+        <script>
+          converse.initialize({
+            // "bosh_service_url": 'https://conversejs.org/http-bind/', // Please use this connection manager only for testing purposes
+            // 'show_controlbox_by_default': true
+          });
+        </script>
+      </Helmet>
+        */}
+
       <Menubar />
 
       <Segment secondary>
