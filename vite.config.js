5c68db90603bfb3b45ac75bfbc90c425d55c036d
Artem Arakcheev <artarakcheev@gmail.com>
2025-06-26 15:08:52 +0300

    Remove obsolete commented code

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { resolve } from "node:path";
 import { readFileSync } from "node:fs";
-import {
-  defineConfig, loadEnv,
-  // createFilter, transformWithEsbuild
-} from "vite";
+import { defineConfig, loadEnv, } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from '@svgr/rollup'
 
@@ -31,7 +28,6 @@ export default defineConfig(({ mode }) => {
           ],
         },
       }),
-      // svgrPlugin(),
     ],
   };
 });
@@ -154,37 +150,6 @@ function importPrefixPlugin() {
     },
   };
 }
-// In Create React App, SVGs can be imported directly as React components. This is achieved by svgr libraries.
-// https://create-react-app.dev/docs/adding-images-fonts-and-files/#adding-svgs
-// function svgrPlugin() {
-//   const filter = createFilter("**/*.svg");
-//   const postfixRE = /[?#].*$/s;
-//   return {
-//     name: "svgr-plugin",
-//     async transform(code, id) {
-//       if (filter(id)) {
-//         const { transform } = await import("@svgr/core");
-//         const { default: jsx } = await import("@svgr/plugin-jsx");
-//         const filePath = id.replace(postfixRE, "");
-//         const svgCode = readFileSync(filePath, "utf8");
-//         const componentCode = await transform(svgCode, undefined, {
-//           filePath,
-//           caller: {
-//             previousExport: code,
-//             defaultPlugins: [jsx],
-//           },
-//         });
-//         const res = await transformWithEsbuild(componentCode, id, {
-//           loader: "jsx",
-//         });
-//         return {
-//           code: res.code,
-//           map: null,
-//         };
-//       }
-//     },
-//   };
-// }
 
 // Replace %ENV_VARIABLES% in index.html
 // https://vitejs.dev/guide/api-plugin.html#transformindexhtml
