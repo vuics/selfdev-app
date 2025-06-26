cd0a0e6e28b67d2acd71eb6a4abbf76ec16afff9
Artem Arakcheev <artarakcheev@gmail.com>
2025-06-26 17:24:41 +0300

    Add language detector

--- src/i18n.js
+++ src/i18n.js
@@ -1,4 +1,6 @@
 import i18n from "i18next";
+import detector from "i18next-browser-languagedetector";
+import backend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
 // the translations
@@ -27,19 +29,27 @@ const resources = {
 };
 
 i18n
+  .use(detector)
+  .use(backend)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
 
     // lng: "en",
-    lng: "en",
     // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
 
     interpolation: {
       escapeValue: false // react already safes from xss
-    }
+    },
+
+    fallbackLng: "en", // use en if detected lng is not available
+    debug: true,
+
+    // TODO: Send not translated keys to endpoint
+    //       https://www.i18next.com/how-to/extracting-translations
+    saveMissing: false, // send not translated keys to endpoint
   });
 
   export default i18n;
