609aaea427741698509d62661e2f58508698c178
Artem Arakcheev <artarakcheev@gmail.com>
2025-06-27 20:16:34 +0300

    Fix hot module replacement in docker; add eslint.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,48 +4,40 @@ import { defineConfig, loadEnv, } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from '@svgr/rollup'
 
+const arr = (str) => str ? str.split(',') : []
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
+  console.log('defineConfig mode:', mode)
+  const {
+    HOST, PORT, HTTPS, SSL_CRT_FILE, SSL_KEY_FILE, ALLOWED_HOSTS
+  } = loadEnv(mode, ".", [
+    "HOST", "PORT", "HTTPS", "SSL_CRT_FILE", "SSL_KEY_FILE", "ALLOWED_HOSTS"
+  ]);
+  const https = HTTPS === "true";
+  const port = parseInt(PORT || "3690", 10)
+
   return {
     base: '/',
     plugins: [
       react(),
-      devServerPlugin(),
       svgr(),
     ],
     build: {
       sourcemap: true,
       outDir: "build",
     },
-  };
-});
-
-// Setup HOST, SSL, PORT
-// Migration guide: Follow the guides below
-// https://vitejs.dev/config/server-options.html#server-host
-// https://vitejs.dev/config/server-options.html#server-https
-// https://vitejs.dev/config/server-options.html#server-port
-function devServerPlugin() {
-  return {
-    name: "dev-server-plugin",
-    config(_, { mode }) {
-      const { HOST, PORT, HTTPS, SSL_CRT_FILE, SSL_KEY_FILE } = loadEnv(mode, ".", ["HOST", "PORT", "HTTPS", "SSL_CRT_FILE", "SSL_KEY_FILE"]);
-      const https = HTTPS === "true";
-      return {
-        server: {
-          host: HOST || "0.0.0.0",
-          port: parseInt(PORT || "3000", 10),
-          open: false,
-          ...(https &&
-            SSL_CRT_FILE &&
-            SSL_KEY_FILE && {
-            https: {
-              cert: readFileSync(resolve(SSL_CRT_FILE)),
-              key: readFileSync(resolve(SSL_KEY_FILE)),
-            },
-          }),
+    server: {
+      host: HOST || "0.0.0.0",
+      port,
+      open: false,
+      ...(https && SSL_CRT_FILE && SSL_KEY_FILE && {
+        https: {
+          cert: readFileSync(resolve(SSL_CRT_FILE)),
+          key: readFileSync(resolve(SSL_KEY_FILE)),
         },
-      };
+      }),
+      allowedHosts: arr(ALLOWED_HOSTS),
     },
-  };
-}
+  }
+});
