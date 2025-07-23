4117a20d6543f953bc059430663fb3f0b228cf7c
Artem Arakcheev <artarakcheev@gmail.com>
2025-07-23 21:47:28 +0300

    Configure ESLint 9

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
+import jest from 'eslint-plugin-jest'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -14,7 +16,8 @@ export default defineConfig([
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      ecmaVersion: 2020,
+      // ecmaVersion: 2020,
+      ecmaVersion: 'latest',
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
@@ -22,10 +25,26 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+    plugins: {
+      react,
+      jest,
+    },
+    settings: {
+      react: {
+        version: 'detect', // 👈 Fixes the warning
+      },
+    },
     rules: {
+      ...react.configs['recommended'].rules,
+      ...reactHooks.configs['recommended-latest'].rules,
+      ...jest.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off', // if using React 17+
+
       // 'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'no-unused-vars': 'warn',
       'react-refresh/only-export-components': 'off', // 'warn', // or 'error'
+      'react/prop-types': 'off',
+      'react/display-name': 'off',
     },
   },
 ])
