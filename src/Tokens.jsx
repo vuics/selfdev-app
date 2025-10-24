279ef375647baee20fdfe7d2b8c9362c853747bc
Artem Arakcheev <artarakcheev@gmail.com>
2025-10-24 21:34:13 +0300

    Display token balances

--- src/Tokens.jsx
+++ src/Tokens.jsx
@@ -7,8 +7,8 @@ import {
   Loader,
   Message,
   Header,
+  Icon,
   // Button,
-  // Icon,
   // Divider,
 } from 'semantic-ui-react'
 import { useTranslation } from 'react-i18next'
@@ -21,7 +21,8 @@ export default function Tokens () {
   const { t } = useTranslation('Tokens')
   const [ responseError, setResponseError ] = useState('')
   const [ loading, setLoading ] = useState(false)
-  const [ address, setAddress ] = useState('')
+  // const [ address, setAddress ] = useState('')
+  const [ account, setAccount ] = useState({})
 
   const getAccount = async () => {
     setLoading(true)
@@ -33,7 +34,8 @@ export default function Tokens () {
       })
       // console.log('res:', res)
       console.log('res.data:', res.data)
-      setAddress(res.data.address)
+      // setAddress(res.data.address)
+      setAccount(res.data)
     } catch (err) {
       console.error('get settings error:', err);
       return setResponseError(err?.response?.data?.message || t('Error getting user account.'))
@@ -91,11 +93,47 @@ export default function Tokens () {
         <Form>
           <Segment stacked>
             <div>
-              Address:
+              <b>Address:</b>
+              {' '}
+              {account?.address}
+            </div>
+            <br/>
+
+            {/*
+            <div>
+              IdentityId:
             </div>
             <div>
-              {address}
+              {account?.identityId}
             </div>
+            <br/>
+            */}
+
+            <b>Balance:</b>
+            <ul>
+            {account?.balances?.map(b => {
+              const { pool, tokenIndex, uri } = b
+              const foundPool = account?.pools?.find(p => p.id === pool)
+              const { symbol, decimals, type } = foundPool
+              const amount = decimals
+                ? Number(BigInt(b.balance)) / 10 ** decimals
+                : b.balance
+
+              return (
+                <li key={pool + tokenIndex}>
+                  <Icon name={type === 'fungible' ? 'cubes' : 'cube'} />
+                  {amount}
+                  {' '}
+                  {symbol}
+                  {' '}
+                  { b.uri && (<>
+                    {' '}
+                      (token index: {tokenIndex}, URI: {uri})
+                  </>)}
+                </li>
+              )
+            })}
+            </ul>
           </Segment>
         </Form>
       </Segment>
