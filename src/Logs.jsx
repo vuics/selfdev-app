13c7c2f708420f6c38b19abc5266a8009b1a9f7d
Artem Arakcheev <artarakcheev@gmail.com>
2025-11-14 22:37:03 +0300

    Fetch logs from GET /logs api endpoint

--- src/Logs.jsx
+++ src/Logs.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useRef } from 'react'
-// import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import {
   Container,
   Segment,
@@ -27,8 +27,7 @@ import i18n from "i18next";
 import { GraphicWalker, TableWalker } from '@kanaries/graphic-walker';
 
 import Menubar from './components/Menubar'
-// import conf from './conf'
-// import dataSource from './ds-students-service.json'
+import conf from './conf'
 
 export default function Omni () {
   const { t } = useTranslation('Logs')
@@ -38,73 +37,39 @@ export default function Omni () {
   // const [ fields, setFields ] = useState(dataSource.fields)
 
   const [ fields, setFields ] = useState([
-    { "fid": "gender", "semanticType": "nominal", "analyticType": "dimension" },
-    { "fid": "race/ethnicity", "semanticType": "nominal", "analyticType": "dimension" },
-    { "fid": "parental level of education", "semanticType": "nominal", "analyticType": "dimension" },
-    { "fid": "lunch", "semanticType": "nominal", "analyticType": "dimension" },
-    { "fid": "test preparation course", "semanticType": "nominal", "analyticType": "dimension" },
-    { "fid": "math score", "semanticType": "quantitative", "analyticType": "measure" },
-    { "fid": "reading score", "semanticType": "quantitative", "analyticType": "measure" },
-    { "fid": "writing score", "semanticType": "quantitative", "analyticType": "measure" }
+    { "fid": "@timestamp", "semanticType": "nominal", "analyticType": "dimension" },
+    { "fid": "agentId", "semanticType": "nominal", "analyticType": "dimension" },
+    { "fid": "level", "semanticType": "nominal", "analyticType": "dimension" },
+    { "fid": "message", "semanticType": "nominal", "analyticType": "dimension" },
+    { "fid": "userId", "semanticType": "nominal", "analyticType": "dimension" },
   ])
 
-  const [ data, setData ] = useState([
-    {
-      "gender": "female",
-      "race/ethnicity": "group B",
-      "parental level of education": "bachelor's degree",
-      "lunch": "standard",
-      "test preparation course": "none",
-      "math score": 72,
-      "reading score": 72,
-      "writing score": 74
-    },
-    {
-      "gender": "female",
-      "race/ethnicity": "group C",
-      "parental level of education": "some college",
-      "lunch": "standard",
-      "test preparation course": "completed",
-      "math score": 69,
-      "reading score": 90,
-      "writing score": 88
-    },
-    {
-      "gender": "female",
-      "race/ethnicity": "group B",
-      "parental level of education": "master's degree",
-      "lunch": "standard",
-      "test preparation course": "none",
-      "math score": 90,
-      "reading score": 95,
-      "writing score": 93
-    },
-  ])
+  const [ data, setData ] = useState([ ])
 
+  const fetchLogs = async () => {
+    setLoading(true)
+    try {
+      // const res = await axios.get(`${conf.api.url}/logs?skip=${conf.omni.skip}&limit=${conf.omni.limit}`, {
+      const res = await axios.get(`${conf.api.url}/logs?skip=${conf.omni.skip}&limit=${conf.omni.limit}`, {
+        headers: { 'Content-Type': 'application/json' },
+        withCredentials: true,
+      })
+      // console.log('res:', res)
+      console.log('res.data:', res.data)
+      // FIXME:
+      // setFields(res.data.fields)
+      setData(res.data.result)
+    } catch (err) {
+      console.error('fetch logs error:', err);
+      return setResponseError(err?.response?.data?.message || t('Error fetching logs.'))
+    } finally {
+      setLoading(false)
+    }
+  }
 
-  // const fetchData = async () => {
-  //   setLoading(true)
-  //   try {
-  //     const res = await axios.get('https://pub-2422ed4100b443659f588f2382cfc7b1.r2.dev/datasets/ds-students-service.json', {
-  //       headers: { 'Content-Type': 'application/json' },
-  //       withCredentials: true,
-  //     })
-  //     // console.log('res:', res)
-  //     console.log('res.data:', res.data)
-  //     setFields(res.data.fields)
-  //     setData(res.data.dataSource)
-  //   } catch (err) {
-  //     console.error('get bridges error:', err);
-  //     return setResponseError(err?.response?.data?.message || t('Error fetching data.'))
-  //   } finally {
-  //     setLoading(false)
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   fetchData()
-  // }, [])
-
+  useEffect(() => {
+    fetchLogs()
+  }, [])
 
   return (<>
     <Container fluid>
@@ -140,12 +105,14 @@ export default function Omni () {
         i18nLang={i18n.language}
       />
 
+      {/*/}
       <TableWalker
         // style={{ width: '100%', height: '100%' }}
         fields={fields}
         data={data}
         pageSize={1000}
       />
+      {/*/}
 
   </>)
 }
