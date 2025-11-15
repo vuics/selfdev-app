48080dc1df36b1dea168e8a386e9ec5564074a48
Artem Arakcheev <artarakcheev@gmail.com>
2025-11-15 20:50:09 +0300

    Add 2 tabs with Grid and Visualization

--- src/Logs.jsx
+++ src/Logs.jsx
@@ -4,6 +4,7 @@ import {
   Container,
   Loader,
   Message,
+  Tab,
   // Header,
   // Segment,
   // Menu,
@@ -23,9 +24,9 @@ import {
   // Table,
 } from 'semantic-ui-react'
 import { useTranslation } from 'react-i18next'
-// import i18n from "i18next";
+import i18n from "i18next";
 
-import { GraphicWalker, TableWalker } from '@kanaries/graphic-walker';
+import { GraphicWalker } from '@kanaries/graphic-walker';
 import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
 import { AgGridReact } from 'ag-grid-react'
 import { themeQuartz } from "ag-grid-community"
@@ -42,16 +43,18 @@ export default function Omni () {
   const [ responseError, setResponseError ] = useState('')
   const [ loading, setLoading ] = useState(false)
 
-  // const [ fields, setFields ] = useState([
-  //   { "fid": "@timestamp", "semanticType": "nominal", "analyticType": "dimension" },
-  //   { "fid": "agentId", "semanticType": "nominal", "analyticType": "dimension" },
-  //   { "fid": "level", "semanticType": "nominal", "analyticType": "dimension" },
-  //   { "fid": "message", "semanticType": "nominal", "analyticType": "dimension" },
-  //   { "fid": "userId", "semanticType": "nominal", "analyticType": "dimension" },
-  // ])
-  // const [ data, setData ] = useState([ ])
+  // For GraphicWalker
+  const [ fields, ] = useState([
+    { "fid": "@timestamp", "semanticType": "nominal", "analyticType": "dimension" },
+    { "fid": "level", "semanticType": "nominal", "analyticType": "dimension" },
+    { "fid": "agentId", "semanticType": "nominal", "analyticType": "dimension" },
+    { "fid": "archetype", "semanticType": "nominal", "analyticType": "dimension" },
+    { "fid": "name", "semanticType": "nominal", "analyticType": "dimension" },
+    { "fid": "message", "semanticType": "nominal", "analyticType": "dimension" },
+  ])
 
-  const [colDefs, setColDefs] = useState([
+  // For AgGridReact
+  const [colDefs, ] = useState([
       { "field": "@timestamp", filter: true },
       { "field": "level", filter: true },
       { "field": "agentId", filter: true },
@@ -107,34 +110,47 @@ export default function Omni () {
 
     </Container>
 
-      {/*/}
-      <GraphicWalker
-        data={data}
-        fields={fields}
-        // pageSize={1000}
-        // chart={graphicWalkerSpec}
-        i18nLang={i18n.language}
+      <Tab
+        menu={{ attached: 'bottom' }} 
+        panes={[ {
+          menuItem: 'Grid',
+          render: () => (
+            <Tab.Pane
+              attached='top'
+              // attached='bottom'
+              // style={{ margin: '0 0 0 0', padding: '0 0 0 0'}}
+            >
+              <div
+                style={{
+                  width: width - 25,
+                  height: height - conf.iframe.topOffset - conf.iframe.bottomOffset - 75
+                }}
+              >
+                <AgGridReact
+                  rowData={rowData}
+                  columnDefs={colDefs}
+                  theme={theme}
+                  pagination
+                  paginationPageSize={500}
+                  paginationPageSizeSelector={[200, 500, 1000]}
+                />
+              </div>
+            </Tab.Pane>),
+        }, {
+          menuItem: 'Visualization',
+          render: () => (
+            <Tab.Pane
+              attached='top'
+              // attached='bottom'
+            >
+              <GraphicWalker
+                data={rowData}
+                fields={fields}
+                i18nLang={i18n.language}
+              />
+            </Tab.Pane>),
+        } ] }
       />
-      <TableWalker
-        // style={{ width: '100%', height: '100%' }}
-        fields={fields}
-        data={data}
-        pageSize={1000}
-      />
-      {/*/}
-
-    <div
-      style={{ width: width, height: height - conf.iframe.topOffset - conf.iframe.bottomOffset }}
-    >
-      <AgGridReact
-        rowData={rowData}
-        columnDefs={colDefs}
-        theme={theme}
-        pagination
-        paginationPageSize={500}
-        paginationPageSizeSelector={[200, 500, 1000]}
-      />
-    </div>
 
   </>)
 }
