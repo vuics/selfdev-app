a6c0b2ffb57a5736a937ad3c48ec0a2b32a0f9c4
Artem Arakcheev <artarakcheev@gmail.com>
2025-11-16 22:41:26 +0300

    Add metrics chart and GraphicWalker

--- src/Logs.jsx
+++ src/Logs.jsx
@@ -25,6 +25,7 @@ import {
 } from 'semantic-ui-react'
 import { useTranslation } from 'react-i18next'
 import i18n from "i18next";
+import { ResponsiveLine } from '@nivo/line'
 
 import { GraphicWalker } from '@kanaries/graphic-walker';
 import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
@@ -37,6 +38,32 @@ import conf from './conf'
 
 ModuleRegistry.registerModules([AllCommunityModule])
 
+
+export function transformPrometheusRange(response) {
+  const result = response?.data?.result?.[0];
+  if (!result) return [];
+
+  const labels = result.metric; // metric labels like job, instance, etc.
+
+  return result.values.map(([ts, val]) => ({
+      timestamp: ts * 1000,
+      value: Number(val),
+      ...labels
+    }));
+}
+
+export function toNivoLineData(points) {
+  return [
+    {
+      id: points[0]?.__name__ || "metric",
+      data: points.map(p => ({
+        x: new Date(p.timestamp),
+        y: p.value
+      }))
+    }
+  ];
+}
+
 export default function Omni () {
   const { t } = useTranslation('Logs')
   const { height, width } = useWindowDimensions();
@@ -44,7 +71,7 @@ export default function Omni () {
   const [ loading, setLoading ] = useState(false)
 
   // For GraphicWalker
-  const [ fields, ] = useState([
+  const [ logsFields, ] = useState([
     { "fid": "@timestamp", "semanticType": "nominal", "analyticType": "dimension" },
     { "fid": "level", "semanticType": "nominal", "analyticType": "dimension" },
     { "fid": "agentId", "semanticType": "nominal", "analyticType": "dimension" },
@@ -54,7 +81,7 @@ export default function Omni () {
   ])
 
   // For AgGridReact
-  const [colDefs, ] = useState([
+  const [logsColumns, ] = useState([
       { "field": "@timestamp", filter: true },
       { "field": "level", filter: true },
       { "field": "agentId", filter: true },
@@ -62,7 +89,18 @@ export default function Omni () {
       { "field": "name", filter: true },
       { "field": "message", filter: true },
   ]);
-  const [rowData, setRowData] = useState([ ]);
+
+  const [ logsData, setLogsData ] = useState([ ]);
+
+  // For GraphicWalker
+  const [ metricsFields, ] = useState([
+    { "fid": "x", "semanticType": "nominal", "analyticType": "dimension" },
+    { "fid": "y", "semanticType": "nominal", "analyticType": "dimension" },
+  ])
+
+  const [ metricsData, setMetricsData ] = useState([]);
+  // console.log('metricsData:', metricsData)
+
 
   const fetchLogs = async () => {
     setLoading(true)
@@ -73,7 +111,7 @@ export default function Omni () {
       })
       // console.log('res:', res)
       console.log('res.data:', res.data)
-      setRowData(res.data.rowData)
+      setLogsData(res.data.logsData)
     } catch (err) {
       console.error('fetch logs error:', err);
       return setResponseError(err?.response?.data?.message || t('Error fetching logs.'))
@@ -82,8 +120,28 @@ export default function Omni () {
     }
   }
 
+  const fetchMetrics = async () => {
+    setLoading(true)
+    try {
+      const res = await axios.get(`${conf.api.url}/logs/metrics`, {
+        headers: { 'Content-Type': 'application/json' },
+        withCredentials: true,
+      })
+      // console.log('res:', res)
+      console.log('res.data:', res.data)
+      const points = transformPrometheusRange(res.data.metrics);
+      setMetricsData(toNivoLineData(points))
+    } catch (err) {
+      console.error('fetch metrics error:', err);
+      return setResponseError(err?.response?.data?.message || t('Error fetching metrics.'))
+    } finally {
+      setLoading(false)
+    }
+  }
+
   useEffect(() => {
     fetchLogs()
+    fetchMetrics()
   }, [])
 
   const theme = themeQuartz.withParams({
@@ -111,9 +169,9 @@ export default function Omni () {
     </Container>
 
       <Tab
-        menu={{ attached: 'bottom' }} 
+        menu={{ attached: 'bottom' }}
         panes={[ {
-          menuItem: 'Grid',
+          menuItem: 'Logs Grid',
           render: () => (
             <Tab.Pane
               attached='top'
@@ -127,8 +185,8 @@ export default function Omni () {
                 }}
               >
                 <AgGridReact
-                  rowData={rowData}
-                  columnDefs={colDefs}
+                  rowData={logsData}
+                  columnDefs={logsColumns}
                   theme={theme}
                   pagination
                   paginationPageSize={500}
@@ -137,15 +195,68 @@ export default function Omni () {
               </div>
             </Tab.Pane>),
         }, {
-          menuItem: 'Visualization',
+          menuItem: 'Metrics Chart',
+          render: () => (
+            <Tab.Pane
+              attached='top'
+              // attached='bottom'
+            >
+              <div
+                style={{
+                  width: width - 25,
+                  height: height - conf.iframe.topOffset - conf.iframe.bottomOffset - 75
+                }}
+              >
+                <ResponsiveLine
+                  data={metricsData}
+                  margin={{ top: 30, right: 30, bottom: 50, left: 60 }}
+                  xScale={{ type: 'time', format: 'native' }}
+                  xFormat="time:%H:%M:%S"
+                  // yScale={{ type: 'linear', min: 'auto', max: 'auto' }}
+                  axisBottom={{
+                    format: "%H:%M:%S",
+                    tickValues: "every 2 minutes",
+                    tickRotation: -30,
+                    legend: "time",
+                    legendOffset: 36
+                  }}
+                  // axisLeft={{
+                  //   tickSize: 5,
+                  //   tickPadding: 5,
+                  //   format: value => Number(value).toFixed(0), // <-- numbers on Y axis
+                  //   legend: "count",
+                  //   legendOffset: -40
+                  // }}
+                  // enableGridX={true} // vertical grid lines
+                  // enableGridY={true} // horizontal grid lines
+                  pointSize={4}
+                  useMesh={true}
+                />
+              </div>
+            </Tab.Pane>),
+        }, {
+          menuItem: 'Logs Analysis',
           render: () => (
             <Tab.Pane
               attached='top'
               // attached='bottom'
             >
               <GraphicWalker
-                data={rowData}
-                fields={fields}
+                data={logsData}
+                fields={logsFields}
+                i18nLang={i18n.language}
+              />
+            </Tab.Pane>),
+        }, {
+          menuItem: 'Metrics Analysis',
+          render: () => (
+            <Tab.Pane
+              attached='top'
+              // attached='bottom'
+            >
+              <GraphicWalker
+                data={metricsData[0].data}
+                fields={metricsFields}
                 i18nLang={i18n.language}
               />
             </Tab.Pane>),
