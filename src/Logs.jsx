4509523cb1c8ec1bfc630f9176cd5d9d1918533b
Artem Arakcheev <artarakcheev@gmail.com>
2025-11-17 16:42:51 +0300

    Add filters and time range with local timezone for log

--- src/Logs.jsx
+++ src/Logs.jsx
@@ -13,9 +13,10 @@ import {
   Segment,
   Menu,
   Card,
+  Dropdown,
+  Modal,
   // Header,
   // Checkbox,
-  // Dropdown,
   // Popup,
   // Divider,
   // Accordion,
@@ -29,6 +30,12 @@ import { useTranslation } from 'react-i18next'
 
 import Form from '@rjsf/semantic-ui'
 import validator from '@rjsf/validator-ajv8';
+
+// import { HighlightStyle, tags } from "@uiw/codemirror-themes-all"
+// import { HighlightStyle, tags } from "@codemirror/language"; // use language package
+// import { Extension } from "@codemirror/state";
+import CodeMirror from '@uiw/react-codemirror';
+import { JsonEditor } from "json-edit-react"
 
 import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
 import { AgGridReact } from 'ag-grid-react'
@@ -62,6 +69,20 @@ import conf from './conf'
 
 ModuleRegistry.registerModules([AllCommunityModule])
 
+
+// basic keywords for DQL
+// const dqlKeywords = ["AND", "OR", "NOT", "level", "name", "message"];
+
+// HighlightStyle for CodeMirror 6
+// export const dqlHighlightStyle = HighlightStyle.define([
+//   { tag: tags.keyword, color: "#d73a49", fontWeight: "bold" },    // AND, OR, NOT
+//   { tag: tags.propertyName, color: "#005cc5" },                  // level:, name:, message:
+//   { tag: tags.string, color: "#032f62" },                        // "quoted string"
+// ]);
+
+// export function dqlSyntaxHighlight() {
+//   return [dqlHighlightStyle];
+// }
 
 
 const prometheusDatasource = {
@@ -200,6 +221,52 @@ export default function Logs () {
   const [ querying, setQuerying ] = useState(false)
   const [ logsQuery, setLogsQuery ] = useState('*')
   const [ adding, setAdding ] = useState(false)
+  const [ selectedLog, setSelectedLog ] = useState(null)
+  const [ modalOpen, setModalOpen ] = useState(false)
+  const [ levelFilter, setLevelFilter ] = useState('')
+  const [ nameFilter, setServiceFilter ] = useState('')
+
+
+  function toLocalDatetime(date) {
+    const pad = (n) => n.toString().padStart(2, "0");
+    return `${date.getFullYear()}-${pad(date.getMonth()+1)}-${pad(date.getDate())}T${pad(date.getHours())}:${pad(date.getMinutes())}`;
+  }
+  const [startTs, setStartTs] = useState(toLocalDatetime(new Date(Date.now() - 24*60*60*1000))); // 24h ago
+  const [endTs, setEndTs] = useState(toLocalDatetime(new Date())); // now
+  function formatLocalTimestamp(utcString) {
+    const date = new Date(utcString);
+    // Example: "2025-11-17 14:59"
+    return date.toLocaleString([], { 
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: false,
+    });
+  }
+
+  // const [startTs, setStartTs] = useState(
+  //   new Date(Date.now() - 24 * 60 * 60 * 1000).toISOString().slice(0,16)
+  // );
+  // const [endTs, setEndTs] = useState(
+  //   new Date().toISOString().slice(0,16)
+  // );
+
+  console.log('startTs:', startTs)
+  console.log('endTs:', endTs)
+  // console.log('selectedLog:', selectedLog)
+
+  // TODO: edit it
+  const [ logsQueryBase, setLogsQueryBase ] = useState('*')
+
+  useEffect(() => {
+    let q = logsQueryBase;
+    if (levelFilter) q += ` AND level:${levelFilter}`;
+    if (nameFilter) q += ` AND name:${nameFilter}`;
+    setLogsQuery(q);
+  }, [levelFilter, nameFilter, logsQueryBase]);
 
   const metricsQueriesSchema = {
     type: 'array',
@@ -216,12 +283,17 @@ export default function Logs () {
 
   // For AgGridReact
   const [logsColumns, ] = useState([
-      { "field": "@timestamp", filter: true },
-      { "field": "level", filter: true },
-      { "field": "agentId", filter: true },
-      { "field": "archetype", filter: true },
-      { "field": "name", filter: true },
-      { "field": "message", filter: true },
+    // { "field": "@timestamp", filter: true },
+    { field: "@timestamp",
+      headerName: "Timestamp",
+      valueFormatter: (params) => formatLocalTimestamp(params.value),
+      filter: true,
+    },
+    { "field": "level", filter: true },
+    { "field": "agentId", filter: true },
+    { "field": "archetype", filter: true },
+    { "field": "name", filter: true },
+    { "field": "message", filter: true },
   ]);
 
   const [ logsData, setLogsData ] = useState([ ]);
@@ -269,7 +341,7 @@ export default function Logs () {
     setLoading(true)
     setQuerying(true)
     try {
-      const res = await axios.get(`${conf.api.url}/logs?skip=${conf.logs.skip}&limit=${conf.logs.limit}&q=${logsQuery}`, {
+      const res = await axios.get(`${conf.api.url}/logs?skip=${conf.logs.skip}&limit=${conf.logs.limit}&q=${logsQuery}&startTs=${new Date(startTs).toISOString()}&endTs=${new Date(endTs).toISOString()}`, {
         headers: { 'Content-Type': 'application/json' },
         withCredentials: true,
       })
@@ -374,6 +446,58 @@ export default function Logs () {
                   </Button>
                 </Input>
 
+
+                <CodeMirror
+                  value={logsQuery}
+                  height="50px"
+                  // extensions={[dqlSyntaxHighlight()]}
+                  onChange={(value) => setLogsQuery(value)}
+                />
+                <Button
+                  icon
+                  color={conf.style.color0}
+                  onClick={fetchLogs}
+                >
+                  <Icon name="search" /> Query
+                </Button>
+
+
+                <Dropdown
+                  placeholder="Level"
+                  selection
+                  clearable
+                  options={aggs?.levels?.buckets?.map(l => ({ key: l.key, text: l.key, value: l.key })) || []}
+                  value={levelFilter}
+                  onChange={(e, { value }) => setLevelFilter(value)}
+                />
+
+                <Dropdown
+                  placeholder="Name"
+                  selection
+                  clearable
+                  options={aggs?.names?.buckets?.map(s => ({ key: s.key, text: s.key, value: s.key })) || []}
+                  value={nameFilter}
+                  onChange={(e, { value }) => setServiceFilter(value)}
+                />
+
+                <Input
+                  type="datetime-local"
+                  label="Start"
+                  value={startTs || ""}
+                  onChange={(e) => setStartTs(e.target.value)}
+                  style={{ marginRight: "10px" }}
+                />
+                <Input
+                  type="datetime-local"
+                  label="End"
+                  value={endTs || ""}
+                  onChange={(e) => setEndTs(e.target.value)}
+                  style={{ marginRight: "10px" }}
+                />
+                <Button onClick={fetchLogs}>Apply Time Range</Button>
+
+
+
                 {aggs && (
                   <LogsHistogram buckets={aggs.per_minute.buckets} />
                 )}
@@ -385,7 +509,23 @@ export default function Logs () {
                   pagination
                   paginationPageSize={500}
                   paginationPageSizeSelector={[200, 500, 1000]}
+
+                  cacheBlockSize={500}
+                  // rowModelType="infinite"
+                  onRowClicked={(e) => { console.log('e:', e); console.log('e.data:', e.data); setSelectedLog(e.data); setModalOpen(true); }}
                 />
+
+                <Modal open={modalOpen} onClose={() => setModalOpen(false)}>
+                  <Modal.Header>Log Detail</Modal.Header>
+                  <Modal.Content>
+                    <JsonEditor
+                      data={selectedLog || {}}
+                      viewOnly
+                      rootName=''
+                    />
+                  </Modal.Content>
+                </Modal>
+
               </div>
             </Tab.Pane>),
         }, {
